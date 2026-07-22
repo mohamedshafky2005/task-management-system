8db59bac2864d3748a29151601764032dbae52a5
Mohamed Shafky <mohamedshafky2005@gmail.com>
2026-07-22 20:17:48 +0530

    Configure frontend production API

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,21 +1,27 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL:
+        import.meta.env.VITE_API_URL || "http://localhost:5000/api",
     headers: {
         "Content-Type": "application/json",
     },
 });
 
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-
-    return config;
-});
+);
 
 api.interceptors.response.use(
     (response) => response,
